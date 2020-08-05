e171096f385cff60d5ab99799ef1a9d58b0302c5
elturcofarid <elturcofarid@gmail.com>
2020-08-04 21:20:25 -0500

    Add files via upload

---
+++ src/test/java/co/gov/serviciocomunes/ServicioUsuariosComunesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.gov.serviciocomunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuariosComunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
